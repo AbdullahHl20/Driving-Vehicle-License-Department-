d083f17b3b17b86bfd5503f36a8acc03fd0d6c47
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-02-22 20:54:58 +0300

    complet cal person

--- DrivingVehicleLicenseDepartment/BusinessLayerDrivingVehicleLicenseDepartment/clsPerson.cs
+++ DrivingVehicleLicenseDepartment/BusinessLayerDrivingVehicleLicenseDepartment/clsPerson.cs
@@ -2,12 +2,24 @@
 {
     public class clsPerson
     {
+
+        public clsPerson()
+        {
+            
+        }
         public int PersonId { get; set; }
 
         public string? NationalNo { get; set; }
         public string? FirstName { get; set; }
         public string? SecondName { get; set; }
         public string? ThirdName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime  DateOfBirth { get; set; }
+        public byte Gendor { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public int? NationalityCountryID { get; set; }
+        public string? ImagePath { get; set; }
         
         
 
