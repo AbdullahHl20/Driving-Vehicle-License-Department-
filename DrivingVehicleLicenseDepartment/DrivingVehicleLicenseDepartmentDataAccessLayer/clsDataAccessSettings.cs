dbf5fce3530ea8b234feeedac6b5ee7a9a3d1cf9
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-02-23 22:09:01 +0300

    Add ctrl Person info Add Icons and create it

---
+++ DrivingVehicleLicenseDepartment/DrivingVehicleLicenseDepartmentDataAccessLayer/clsDataAccessSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrivingVehicleLicenseDepartmentDataAccessLayer
+{
+    public class clsDataAccessSettings
+    {
+        public static string ConnectionString = "server=ABDULLAH\\MSSQLSERVER22;User id=ABDULLAH\\abdullah;database=DVLD;Integrated Security=True;";
+    }
+}
--- DrivingVehicleLicenseDepartment/DrivingVehicleLicenseDepartmentPresentationLayer/frmMangePeople.cs
+++ DrivingVehicleLicenseDepartment/DrivingVehicleLicenseDepartmentPresentationLayer/frmMangePeople.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayerDrivingVehicleLicenseDepartment;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,44 @@ namespace DrivingVehicleLicenseDepartmentPresentationLayer
 {
     public partial class frmMangePeople : Form
     {
+
+        private DataView dv;
         public frmMangePeople()
         {
             InitializeComponent();
+           
+        }
+
+        private void frmMangePeople_Load(object sender , EventArgs e)
+        {
+            dv = new DataView(clsPerson.GetAllPerson());
+            dataGridView1.DataSource = dv;
+            SetRecordCount(dataGridView1.RowCount);
+            comboBox1.Items.Add("noun");
+
+            foreach (DataColumn dataColumn in  dv.Table.Columns)
+              comboBox1.Items.Add((string)dataColumn.ColumnName);
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender , EventArgs e)
+        {
+           txtFilteredValue.Visible = comboBox1.SelectedIndex != 0;
+        }
+
+        private void txtFilteredValue_TextChanged(object sender , EventArgs e)
+        {
+            dv.RowFilter = "";
+
+            if ( txtFilteredValue.Text != string.Empty )
+                dv.RowFilter = (string)comboBox1.SelectedItem.ToString() + "=" + "\'" +txtFilteredValue.Text + "\'";
+            SetRecordCount(dataGridView1.RowCount);
+        }
+
+        public void SetRecordCount(int recoredCount)
+        {
+            lblRecordCount.Text = $"Records = {recoredCount - 1 }" ;
         }
     }
 }
