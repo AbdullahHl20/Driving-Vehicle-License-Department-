7860162d8159ac8e1a687275033f1a82f46e81da
Abdullah Alhelal <abdullah.h@alameensoft.com>
2025-02-24 04:06:03 -0800

    add image for mange people

--- DrivingVehicleLicenseDepartment/DrivingVehicleLicenseDepartmentPresentationLayer/Properties/Resources.Designer.cs
+++ DrivingVehicleLicenseDepartment/DrivingVehicleLicenseDepartmentPresentationLayer/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace DrivingVehicleLicenseDepartmentPresentationLayer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap management {
+            get {
+                object obj = ResourceManager.GetObject("management", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap People {
             get {
                 object obj = ResourceManager.GetObject("People", resourceCulture);
--- DrivingVehicleLicenseDepartment/DrivingVehicleLicenseDepartmentPresentationLayer/frmMangePeople.Designer.cs
+++ DrivingVehicleLicenseDepartment/DrivingVehicleLicenseDepartmentPresentationLayer/frmMangePeople.Designer.cs
@@ -29,25 +29,41 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(28, 169);
+            this.label1.Location = new System.Drawing.Point(11, 179);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 33);
+            this.label1.Size = new System.Drawing.Size(99, 27);
             this.label1.TabIndex = 0;
             this.label1.Text = "Filter By:";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DrivingVehicleLicenseDepartmentPresentationLayer.Properties.Resources.management;
+            this.pictureBox1.Location = new System.Drawing.Point(394, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(344, 188);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // frmMangePeople
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1663, 635);
+            this.ClientSize = new System.Drawing.Size(1247, 516);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmMangePeople";
             this.Text = "frmMangePeople";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -55,5 +71,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
